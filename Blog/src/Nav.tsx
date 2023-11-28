f00f72774671f60ed79b12bce85adc78fe1d32a5
ElJake0 <jake.m.kent@gmail.com>
2023-11-28 22:59:59 +1100

    Starting small working on Nav

---
+++ Blog/src/Nav.tsx
@@ -0,0 +1,15 @@
+import '../styles/Nav.css'
+
+function Nav() {
+  return (
+    <>
+     <ul>
+        <li><a href="">Blog</a></li>
+        <li><a href="">About Me</a></li>
+        <li><a href="">Contact</a></li>
+      </ul> 
+    </>
+  )
+}
+
+export default Nav
