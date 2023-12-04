27f8a037de3ed588bebd63c6199e6eb4ffb22ce0
CruisingAltitude <18636587@student.curtin.edu.au>
2023-12-04 21:39:53 +1100

    Added article viewmodel:

---
+++ AdminPortal/ViewModels/ArticleVM.cs
@@ -0,0 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace AdminPortal.ViewModels;
+
+public class ArticleVM
+{
+  public int? ArticleId { get; init; }
+  [Required]
+  public string ArticleTitle { get; set; }
+
+  [Required]
+  public string ArticleBody { get; set; }
+
+  public string Status { get; set; }
+
+  public bool Hidden { get; set; }
+}
