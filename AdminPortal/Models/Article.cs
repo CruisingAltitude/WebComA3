bbb02f56da4693c3a0965875a98c84245836d1d2
CruisingAltitude <18636587@student.curtin.edu.au>
2023-12-04 21:37:47 +1100

    Added article model:

---
+++ AdminPortal/Models/Article.cs
@@ -0,0 +1,33 @@
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AdminPortal.Models;
+
+public class Article{
+  [Key]
+  [Required]
+  public int ArticleId { get; init; }
+
+  [ForeignKey("Account")]
+  public int AuthourId { get; init; }
+  public virtual Account Authour { get; init; }
+
+  [Required]
+  public string ArticleTitle { get; set; }
+
+  public string ArticleBody { get; set; }
+
+  [Required]
+  [DataType(DataType.Date)]
+  public DateTime CreationTimeUTC { get; init; }
+
+  [DataType(DataType.Date)]
+  public DateTime PublishTimeUTC { get; init; }
+
+  public string Status { get; set; }
+
+  [Required]
+  [DefaultValue("false")]
+  public bool Hidden { get; set; }
+}
