7ab06f29e9d792ab32cacd375e3ea9e190b192bb
CruisingAltitude <18636587@student.curtin.edu.au>
2023-12-04 21:46:43 +1100

    Added index method to article controller:

--- AdminPortal/Controllers/ArticleController.cs
+++ AdminPortal/Controllers/ArticleController.cs
@@ -1,9 +1,21 @@
 using Microsoft.EntityFrameworkCore;
 using AdminPortal.Models;
 using AdminPortal.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace AdminPortal.Controllers;
 
 public class ArticleController{
+  private readonly DbContext _context;
+  
+  public ArticleController(DbContext context){
+    _context = context;
+  }
 
+  public async Task<IActionResult> Index()
+  {
+      var portfolioDbContext = await _context.Articles.Include(a => a.Account);
+      return View(portfolioDbContext.ToListAsync());
+  }
 }
