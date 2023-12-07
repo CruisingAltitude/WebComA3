a263cfc95ac019edcbc98a01cdfb4f6b06c99d9f
CruisingAltitude <18636587@student.curtin.edu.au>
2023-12-07 21:48:46 +1100

    added api function to get single articl:

--- API/Controllers/ArticleController.cs
+++ API/Controllers/ArticleController.cs
@@ -7,21 +7,31 @@ using AdminPortal.Models;
 [ApiController]
 public class ArticleController : ControllerBase
 {
-    private readonly AdminPortalDbContext _context;
+  private readonly AdminPortalDbContext _context;
 
-    public ArticleController(AdminPortalDbContext context)
-    {
-        _context = context;
-    }
+  public ArticleController(AdminPortalDbContext context)
+  {
+      _context = context;
+  }
 
-    // GET: api/Article
-    [HttpGet]
-    public async Task<ActionResult<IEnumerable<Article>>> GetArticle()
+  // GET: api/Article
+  [HttpGet]
+  public async Task<ActionResult<IEnumerable<Article>>> GetArticleList()
+  {
+    if (_context.Articles == null)
     {
-      if (_context.Articles == null)
-      {
-          return NotFound();
-      }
-      return await _context.Articles.ToListAsync();
+        return NotFound();
     }
+    return await _context.Articles.ToListAsync();
+  }
+
+  [HttpGet("{id}")]
+  public async Task<ActionResult<IEnumerable<Article>>> GetArticle(int id)
+  {
+    if (_context.Articles == null)
+    {
+        return NotFound();
+    }
+    return await _context.Articles.Where(x => x.ArticleId == id).ToListAsync();
+  }
 }
