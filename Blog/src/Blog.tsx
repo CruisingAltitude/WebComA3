afc8e037f3b50a387dd3e06d1ec5e8f9930c2d19
CruisingAltitude <18636587@student.curtin.edu.au>
2023-12-07 23:25:33 +1100

    Working on blog smmary cards:

--- Blog/src/Blog.tsx
+++ Blog/src/Blog.tsx
@@ -1,6 +1,40 @@
+import { useState, useEffect } from 'react'
+import '../styles/Blog.css'
+
 function Blog(){
+  const url = "http://localhost:5177/api/article";
+  const [data, setData] = useState([]);
+
+  function getData(){
+    fetch(url)
+      .then(function(response){
+        return response.json();
+      })
+      .then(function(myJson) {
+        setData(myJson)
+      });
+  }
+
+  useEffect(()=>{
+    getData()
+  },[])
+
   return (
-    <h1>Blog</h1>
+    <>
+      <h1>Blog</h1>
+      <div className="card-gallery">
+      {
+        // data.map((item) => <li key={item['articleId']}>{item['articleTitle']} {item['creationTimeUTC']}</li>)
+        data.map((item) => 
+        <div className="card">
+          <div className="card-body">
+            <h5 className="card-title">{item['articleTitle']}</h5>
+            <p className="card-text">{item['articleSummary']}</p>
+          </div>
+        </div>)
+      }
+      </div>
+     </>
   )
 }
 
